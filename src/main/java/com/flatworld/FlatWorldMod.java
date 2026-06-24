c965522c137dcf575379f7187678fe606c38d0e0
muizzzxd-code <muizzzxd@gmail.com>
2026-06-24 12:43:11 +0700

    Create FlatWorldMod.java

---
+++ src/main/java/com/flatworld/FlatWorldMod.java
@@ -0,0 +1,8 @@
+package com.flatworld;
+import net.fabricmc.api.ModInitializer;
+public class FlatWorldMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Flat World Mod aktif!");
+    }
+}
